0e650d23033ac04301f7d6866b70d4055f14fafb
timpim-dev <fake.felix@protonmail.com>
2026-06-04 18:23:35 +0200

    Add initial dirt path tileset and project/session files

---
+++ dirt path.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="dirt path" tilewidth="16" tileheight="16" tilecount="55" columns="11">
+ <image source="../felixx.dev-arpg/world/dirt_tileset.png" width="176" height="80"/>
+</tileset>
